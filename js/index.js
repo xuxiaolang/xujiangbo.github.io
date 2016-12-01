875d24b8516ce9698eac5cf56bf25a3cade8f7f2
xuxiaolang <m18793473245@163.com>
2016-12-01 18:20:27 +0800

    测试index.js代码1

--- js/index.js
+++ js/index.js
@@ -2,10 +2,8 @@
 * @Author: to exercise self-con
 * @Date:   2016-12-01 17:28:20
 * @Last Modified by:   to exercise self-con
-* @Last Modified time: 2016-12-01 18:16:58
+* @Last Modified time: 2016-12-01 18:18:28
 */
-
-'use strict';
 
 
 $(function(){
