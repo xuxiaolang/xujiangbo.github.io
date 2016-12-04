0cb52c7d5b86072314b058f0f4dea9a2b6c7f4c3
xuxiaolang <m18793473245@163.com>
2016-12-04 16:32:11 +0800

    提交js文件

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@
 * @Author: to exercise self-con
 * @Date:   2016-12-01 17:28:20
 * @Last Modified by:   to exercise self-con
-* @Last Modified time: 2016-12-01 18:18:28
+* @Last Modified time: 2016-12-01 23:45:14
 */
+
+'use strict';
 
 
 $(function(){
-	console.log("我是测试的代码");
+	console.log("我是测试代码");
 })
